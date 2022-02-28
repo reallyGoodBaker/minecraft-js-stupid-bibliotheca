46671a15136037a12123ea512f388e11b3cdf55d
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2022-02-28 22:23:32 +0800

    Update tellraw-console.js

--- dist/tellraw-console.js
+++ dist/tellraw-console.js
@@ -24,7 +24,7 @@ const Formatting = {
 };
 
 const tab = new Array(2).fill(' ').join('');
-const getTab = count => new Array(count).fill(tab).join('');
+const getTab = (count=1) => new Array(count).fill(tab).join('');
 
 class MsgBlock extends Array {
     static defaultColor = Formatting.white;
@@ -114,112 +114,6 @@ function basicTypeParser(data, type) {
     }
 
     return mbf('', color, data.toString());
-}
-
-function fakeNativeToString(name, ...args) {
-    function toString() { return `function ${name}(${args.join(', ')}) { [native code] }`}
-    return toString;
-}
-
-class RawTeller {
-    static sender = null;
-    /**
-     * @type {[string, string, any][]}
-     */
-    msgQueue = [];
-    pending = false;
-
-    static header = '';
-    /**
-     * @type {RawTeller}
-     */
-    static rawTeller;
-    
-    constructor(header) {
-        this.header = header || RawTeller.header;
-        RawTeller.rawTeller = this;
-    }
-
-    send(msg, selector='@a[tag=debugger]') {
-        this.msgQueue.push([selector, msg, RawTeller.sender]);
-    }
-
-    pend() {
-        this.pending = true;
-    }
-
-    active() {
-        if (this.pending) return;
-
-        this.msgQueue.forEach(msg => {
-            const [selector, message, sender] = msg;
-            sender.runCommand(`tellraw ${selector} {"rawtext": [{"text": "${this.header} ${message}"}]}`);
-        });
-
-        this.msgQueue = [];
-    }
-
-    setSender(s) {
-        RawTeller.sender = s;
-    }
-
-}
-
-// const untrustedHeader = mbf('', style('red'), '[Untrusted] ');
-
-// export class UntrustedRawTeller extends RawTeller {
-
-//     constructor(header) {
-//         super(header);
-
-//         this.send = this.sendUntrusted;
-//     }
-
-//     sendUntrusted(msg, selector='@a[tag=debugger]') {
-//         try {
-//             this.sender.runCommand(`tellraw ${selector} {"rawtext": [{"text": "${untrustedHeader + msg}"}]}`);
-//         } catch (error) {
-//             this.onError.call(undefined, e, msg, selector);
-//         }
-//     }
-
-//     setSender(s) {
-//         this.sender = s;
-//     }
-
-//     onError(e) {
-//     }
-
-// }
-
-/**
- * @param {any} commander 
- * @returns {Function}
- */
-function getRawTeller(commander) {
-
-    let sender = new RawTeller();
-    sender.setSender(commander);
-
-    function send(msg, selector) {
-        sender.send(msg, selector);
-    }
-
-    send.toString = fakeNativeToString('send', 'msg', 'selector');
-
-    send.update = () => {
-        sender.active();
-    };
-
-    let senderProxy = new Proxy(send, {
-        get(t, p) {
-            return t[p];
-        },
-
-        set() { return false }
-    });
-
-    return senderProxy;
 }
 
 async function toString(obj, showDetails=false, showSetterGetter=false) {
@@ -422,6 +316,85 @@ function doRegisterSpecParsers() {
 
 doRegisterSpecParsers();
 
+function fakeNativeToString(name, ...args) {
+    function toString() { return `function ${name}(${args.join(', ')}) { [native code] }`}
+    return toString;
+}
+
+class RawTeller {
+    static sender = null;
+    /**
+     * @type {[string, string, any][]}
+     */
+    msgQueue = [];
+    pending = false;
+
+    static header = '';
+    /**
+     * @type {RawTeller}
+     */
+    static rawTeller;
+    
+    constructor(header) {
+        this.header = header || RawTeller.header;
+        RawTeller.rawTeller = this;
+    }
+
+    send(msg, selector='@a[tag=debugger]') {
+        this.msgQueue.push([selector, msg, RawTeller.sender]);
+    }
+
+    pend() {
+        this.pending = true;
+    }
+
+    active() {
+        if (this.pending) return;
+
+        this.msgQueue.forEach(msg => {
+            const [selector, message, sender] = msg;
+            sender.runCommand(`tellraw ${selector} {"rawtext": [{"text": "${this.header} ${message}"}]}`);
+        });
+
+        this.msgQueue = [];
+    }
+
+    setSender(s) {
+        RawTeller.sender = s;
+    }
+
+}
+
+/**
+ * @param {any} commander 
+ * @returns {Function}
+ */
+function getRawTeller(commander) {
+
+    let sender = new RawTeller();
+    sender.setSender(commander);
+
+    function send(msg, selector) {
+        sender.send(msg, selector);
+    }
+
+    send.toString = fakeNativeToString('send', 'msg', 'selector');
+
+    send.update = () => {
+        sender.active();
+    };
+
+    let senderProxy = new Proxy(send, {
+        get(t, p) {
+            return t[p];
+        },
+
+        set() { return false }
+    });
+
+    return senderProxy;
+}
+
 class Format {
     /**
      * @type {Format[]}
@@ -522,7 +495,7 @@ function initConsole(commander, selector) {
             let msg = typeof cur === 'object'? await toString(cur, true): 
                 typeof cur === 'string'? mbf('', style(s), safeString(cur)): basicTypeMsg(cur);
 
-            if (i) res.push(getTab(1));
+            if (i) res.push(getTab());
             res.push(msg);
         }
 
