86356f784e4bbbec00a179cc31b09d191b74b484
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2022-03-01 20:36:42 +0800

    Update terminal.js

--- src/tellraw_console/terminal.js
+++ src/tellraw_console/terminal.js
@@ -1,19 +1,44 @@
 import { mbf, style, getTab } from "./msgblock.js";
 import { RawTeller } from "./sender.js";
+import {EventEmitter} from '../events.js'
 
-const UNTRUSTED_HEADER = '[Untrusted]';
+const UNTRUSTED_HEADER = 'Untrusted >';
 const UNTRUSTED_HEADER_PREFIX = mbf(style('italic'), style('red'), UNTRUSTED_HEADER);
 
 function sendUntrusted(msg, selector) {
     RawTeller.rawTeller.send(UNTRUSTED_HEADER_PREFIX + getTab() + msg, selector);
 }
 
+function send(msg, selector) {
+    RawTeller.rawTeller.send(msg, selector);
+}
+
 let commandRegistry = {};
 
-export function register(command, opt) {
-    commandRegistry[command] = opt;
+/**
+ * @param {string} command 
+ * @param {(em: EventEmitter)=>void} handler 
+ * @param {{thisArg?: any;captureRejections?: boolean;}} [opt]
+ */
+export function register(command, handler, opt) {
+    let em = new EventEmitter(opt);
+    commandRegistry[command] = em;
+    handler(em);
 }
 
 export function exec(commandStr) {
-    
+    let args = commandStr.split(/\s+/g);
+
+}
+
+class Context {
+    stack = [];
+
+    constructor(obj, initializer) {
+        this._savedData = initializer.call(this, obj);
+    }
+
+    add(data) {
+        
+    }
 }
